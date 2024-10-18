c3eefc1ea11d17b3502f8181825fcc70b07ac993
thendont <165030137+thendontuser@users.noreply.github.com>
2024-10-19 01:03:29 +0300

    Add files via upload
    changing the font of the text and the size of the window

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -42,10 +42,12 @@
             CodeSection.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             CodeSection.BackColor = Color.FromArgb(0, 0, 64);
             CodeSection.BorderStyle = BorderStyle.FixedSingle;
+            CodeSection.Font = new Font("Microsoft YaHei", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
             CodeSection.ForeColor = SystemColors.ControlLightLight;
-            CodeSection.Location = new Point(12, 47);
+            CodeSection.Location = new Point(16, 54);
+            CodeSection.Margin = new Padding(4, 3, 4, 3);
             CodeSection.Name = "CodeSection";
-            CodeSection.Size = new Size(1156, 415);
+            CodeSection.Size = new Size(1317, 475);
             CodeSection.TabIndex = 0;
             CodeSection.Text = "";
             CodeSection.TextChanged += CodeSection_TextChanged;
@@ -53,9 +55,10 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(12, 465);
+            label1.Location = new Point(16, 535);
+            label1.Margin = new Padding(4, 0, 4, 0);
             label1.Name = "label1";
-            label1.Size = new Size(55, 20);
+            label1.Size = new Size(65, 23);
             label1.TabIndex = 1;
             label1.Text = "Output";
             // 
@@ -63,11 +66,14 @@
             // 
             OutputBox.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             OutputBox.BackColor = Color.FromArgb(0, 0, 64);
+            OutputBox.Font = new Font("Microsoft YaHei", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
             OutputBox.ForeColor = SystemColors.Window;
             OutputBox.FormattingEnabled = true;
-            OutputBox.Location = new Point(12, 488);
+            OutputBox.ItemHeight = 27;
+            OutputBox.Location = new Point(16, 561);
+            OutputBox.Margin = new Padding(4, 3, 4, 3);
             OutputBox.Name = "OutputBox";
-            OutputBox.Size = new Size(1156, 124);
+            OutputBox.Size = new Size(1317, 112);
             OutputBox.TabIndex = 2;
             // 
             // RunBtn
@@ -75,9 +81,10 @@
             RunBtn.Anchor = AnchorStyles.Top | AnchorStyles.Right;
             RunBtn.BackColor = SystemColors.ActiveBorder;
             RunBtn.ForeColor = SystemColors.ActiveCaptionText;
-            RunBtn.Location = new Point(1074, 12);
+            RunBtn.Location = new Point(1206, 14);
+            RunBtn.Margin = new Padding(4, 3, 4, 3);
             RunBtn.Name = "RunBtn";
-            RunBtn.Size = new Size(94, 29);
+            RunBtn.Size = new Size(129, 33);
             RunBtn.TabIndex = 3;
             RunBtn.Text = "Run";
             RunBtn.UseVisualStyleBackColor = false;
@@ -87,9 +94,10 @@
             // 
             OpenBtn.BackColor = SystemColors.ActiveBorder;
             OpenBtn.ForeColor = SystemColors.ActiveCaptionText;
-            OpenBtn.Location = new Point(12, 12);
+            OpenBtn.Location = new Point(16, 14);
+            OpenBtn.Margin = new Padding(4, 3, 4, 3);
             OpenBtn.Name = "OpenBtn";
-            OpenBtn.Size = new Size(94, 29);
+            OpenBtn.Size = new Size(129, 33);
             OpenBtn.TabIndex = 4;
             OpenBtn.Text = "Open";
             OpenBtn.UseVisualStyleBackColor = false;
@@ -99,9 +107,10 @@
             // 
             SaveBtn.BackColor = SystemColors.ActiveBorder;
             SaveBtn.ForeColor = SystemColors.ActiveCaptionText;
-            SaveBtn.Location = new Point(112, 12);
+            SaveBtn.Location = new Point(154, 14);
+            SaveBtn.Margin = new Padding(4, 3, 4, 3);
             SaveBtn.Name = "SaveBtn";
-            SaveBtn.Size = new Size(94, 29);
+            SaveBtn.Size = new Size(129, 33);
             SaveBtn.TabIndex = 5;
             SaveBtn.Text = "Save";
             SaveBtn.UseVisualStyleBackColor = false;
@@ -109,19 +118,21 @@
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(11F, 23F);
             AutoScaleMode = AutoScaleMode.Font;
             AutoSize = true;
             BackColor = Color.FromArgb(0, 0, 64);
-            ClientSize = new Size(1180, 637);
+            ClientSize = new Size(1351, 731);
             Controls.Add(SaveBtn);
             Controls.Add(OpenBtn);
             Controls.Add(RunBtn);
             Controls.Add(OutputBox);
             Controls.Add(label1);
             Controls.Add(CodeSection);
+            Font = new Font("Yu Gothic", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 204);
             ForeColor = SystemColors.ButtonHighlight;
             Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(4, 3, 4, 3);
             Name = "Form1";
             Text = "CalcLang";
             ResumeLayout(false);
